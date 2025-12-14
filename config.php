54b170b854e2b9fd3dff0c4aaad11f4ce38109cc
Huong <huong281203@gmail.com>
2025-12-14 23:31:40 +0700

    push lan 2

--- config.php
+++ config.php
@@ -1,13 +1,17 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "studentdb";
+define("HOST", "sql113.infinityfree.com");          // MySQL Hostname
+define("DATABASE", "if0_40557505_studentdb");       // Database Name
+define("USERNAME", "if0_40557505");                // MySQL Username
+define("PASSWORD", "Honghuong12345");              // MySQL Password
 
-$conn = mysqli_connect($host, $user, $password, $dbname);
-
-if (!$conn) {
-    die("Kết nối database thất bại: " . mysqli_connect_error());
+try {
+    $conn = new PDO(
+        "mysql:host=" . HOST . ";dbname=" . DATABASE . ";charset=utf8",
+        USERNAME,
+        PASSWORD
+    );
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Kết nối database thất bại: " . $e->getMessage());
 }
-
-mysqli_set_charset($conn, "utf8");
+?>
